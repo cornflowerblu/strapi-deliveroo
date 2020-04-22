24d8ff8f7986ff827441ec74133d98260c113c5b
rurich <roger.urich@shiftpixy.com>
2020-04-21 23:02:17 -0700

    add reactstrap and layout

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,14 @@
-export default () => <div>Welcome to next.js!</div>
+import {Button, Alert } from 'reactstrap'
+
+export default () => {
+  return(
+    <div>
+      <div>
+        <Alert color="primary" >
+          Hello this will be a strapi project with React and Bootstrap.
+        </Alert>
+        &nbsp; <Button color="primary">Hello from NextJs.</Button> 
+      </div>
+    </div>
+  );
+}
